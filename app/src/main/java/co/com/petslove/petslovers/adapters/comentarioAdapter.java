7b061de26d221dfa882c4f8e40162b75623294a6
Gian Carlos Guatibonza Suarez <gguatibonza08@gmail.com>
2018-11-06 00:29:43 -0500

    fragmetn red social
    se agregaron los adapters  y el recycler al fragment de red social

--- app/src/main/java/co/com/petslove/petslovers/adapters/comentarioAdapter.java
+++ app/src/main/java/co/com/petslove/petslovers/adapters/comentarioAdapter.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import co.com.petslove.petslovers.R;
 import co.com.petslove.petslovers.model.ComentarioPojo;
@@ -20,10 +21,10 @@ public class comentarioAdapter extends RecyclerView.Adapter<comentarioAdapter.Vi
 
 
     private Context context;
-    private ArrayList<ComentarioPojo> listaComentarios;
+    private List<ComentarioPojo> listaComentarios;
     private View.OnClickListener listener;
 
-    public comentarioAdapter(Context context, ArrayList<ComentarioPojo> listaComentarios) {
+    public comentarioAdapter(Context context, List<ComentarioPojo> listaComentarios) {
         this.context = context;
         this.listaComentarios = listaComentarios;
     }
--- app/src/main/java/co/com/petslove/petslovers/fragments/RedSocial.java
+++ app/src/main/java/co/com/petslove/petslovers/fragments/RedSocial.java
@@ -3,28 +3,39 @@ package co.com.petslove.petslovers.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import co.com.petslove.petslovers.R;
+import co.com.petslove.petslovers.adapters.publicacionAdapter;
+import co.com.petslove.petslovers.model.ComentarioPojo;
+import co.com.petslove.petslovers.model.PublicacionPojo;
 
 
 public class RedSocial extends Fragment {
 
-
+    private RecyclerView redSocial;
     private OnFragmentInteractionListener mListener;
+    private ArrayList<PublicacionPojo> publicaciones;
+    private ArrayList<ComentarioPojo> comentarios;
+    private FloatingActionButton addPublicacion;
 
     public RedSocial() {
 
     }
 
 
-    public static RedSocial newInstance(String param1, String param2) {
+    public static RedSocial newInstance() {
         RedSocial fragment = new RedSocial();
         Bundle args = new Bundle();
-
         fragment.setArguments(args);
         return fragment;
     }
@@ -39,11 +50,32 @@ public class RedSocial extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_red_social, container, false);
+        View view = inflater.inflate(R.layout.fragment_red_social, container, false);
+        redSocial = view.findViewById(R.id.listaPublicacions);
+        addPublicacion = view.findViewById(R.id.addPublicacion);
+
+        addPublicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "me falta agregar la otra activity para esto", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //en este metodo agregar el llamado a web services
+        referenciar();
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    private void referenciar() {
+
+        //configurar conexion a web servies
+
+        publicacionAdapter publicacionAdapter = new publicacionAdapter(getContext(), publicaciones);
+        redSocial.setLayoutManager(new LinearLayoutManager(getContext()));
+        redSocial.setAdapter(publicacionAdapter);
+    }
+
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -67,16 +99,7 @@ public class RedSocial extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);

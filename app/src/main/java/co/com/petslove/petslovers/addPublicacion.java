b5673f6c5c3a96b3e2d8fa63e781a3762f0a09d5
Gian Carlos Guatibonza Suarez <gguatibonza08@gmail.com>
2018-11-14 22:12:51 -0500

    encode base64
    publicar

--- app/src/main/java/co/com/petslove/petslovers/addPublicacion.java
+++ app/src/main/java/co/com/petslove/petslovers/addPublicacion.java
@@ -1,39 +1,30 @@
 package co.com.petslove.petslovers;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 
 public class addPublicacion extends AppCompatActivity {
 
-    private final String CARPETA_RAIZ = "misImagenesPrueba/";
-    private final String RUTA_IMAGEN = CARPETA_RAIZ + "misFotos";
     private String codeFoto;
     private ImageView foto;
     private EditText contenido;
-    String path;
+    private Button publicar;
 
     final int COD_SELECCIONA = 10;
-    final int COD_FOTO = 20;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,31 +32,32 @@ public class addPublicacion extends AppCompatActivity {
         setContentView(R.layout.activity_add_publicacion);
         foto = findViewById(R.id.fotoPublicacionAdd);
         contenido = findViewById(R.id.descripcionPublicacion);
-        cargarImagen();
-    }
-
-    private void cargarImagen() {
-
-        final CharSequence[] opciones = {"Tomar Foto", "Cargar Imagen", "Cancelar"};
-        final AlertDialog.Builder alertOpciones = new AlertDialog.Builder(addPublicacion.this);
-        alertOpciones.setTitle("Seleccione una Opción");
-        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+        publicar = findViewById(R.id.publicar);
+        cargarImagen2();
+        publicar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (opciones[i].equals("Tomar Foto")) {
-                    tomarFotografia();
-                } else {
-                    if (opciones[i].equals("Cargar Imagen")) {
-                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        intent.setType("image/");
-                        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicación"), COD_SELECCIONA);
-                    } else {
-                        dialogInterface.dismiss();
-                    }
-                }
+            public void onClick(View v) {
+                publicar();
             }
         });
-        alertOpciones.show();
+
+    }
+
+    private void publicar() {
+        //conexion
+
+
+
+
+
+
+
+    }
+
+    private void cargarImagen2() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicación"), COD_SELECCIONA);
     }
 
     @Override
@@ -74,55 +66,28 @@ public class addPublicacion extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case COD_SELECCIONA:
+                    Uri imageUri = data.getData();
+                    foto.setImageURI(imageUri);
                     try {
-                        Uri imageUri = data.getData();
-                        Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                        byte[] b = baos.toByteArray();
-                        codeFoto = Base64.encodeToString(b, Base64.DEFAULT);
-                        foto.setImageBitmap(decode64(codeFoto.getBytes()));
-
-
+                        procesar(imageUri);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
-
-
-                /*byte[] decodedBytes = Base64.decode(code.getBytes(), Base64.DEFAULT);
-                Bitmap bn = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
-                bn.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                prueba.setImageBitmap(bn);*/
-
-
-
-
-
-
-                    /*ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    byte[] decodedBytes = Base64.decode(bytes, Base64.DEFAULT);
-                    Bitmap bn = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
-                    bn.compress(Bitmap.CompressFormat.JPEG, 100, baos);*/
-                    break;
-
-                case COD_FOTO:
-                    MediaScannerConnection.scanFile(this, new String[]{path}, null,
-                            new MediaScannerConnection.OnScanCompletedListener() {
-                                @Override
-                                public void onScanCompleted(String path, Uri uri) {
-                                    Log.i("Ruta de almacenamiento", "Path: " + path);
-                                }
-                            });
-
-                 /*   Bitmap bitmap= BitmapFactory.decodeFile(path);
-                    imagen.setImageBitmap(bitmap);*/
-
                     break;
             }
 
 
         }
+    }
+
+    private void procesar(Uri imageUri) throws IOException {
+        Log.e("code", "LLEGO A DECODIFICAR");
+        Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] b = baos.toByteArray();
+        codeFoto = Base64.encodeToString(b, Base64.DEFAULT);
+        Log.e("code", codeFoto);
     }
 
 
@@ -140,31 +105,4 @@ public class addPublicacion extends AppCompatActivity {
 
     }
 
-
-    private void tomarFotografia() {
-        File fileImagen = new File(Environment.getExternalStorageDirectory(), RUTA_IMAGEN);
-        boolean isCreada = fileImagen.exists();
-        String nombreImagen = "";
-        if (isCreada == false) {
-            isCreada = fileImagen.mkdirs();
-        }
-
-        if (isCreada == true) {
-            nombreImagen = (System.currentTimeMillis() / 1000) + ".jpg";
-        }
-
-        path = Environment.getExternalStorageDirectory() +
-                File.separator + RUTA_IMAGEN + File.separator + nombreImagen;
-        File imagen = new File(path);
-        Intent intent = null;
-        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            String authorities = getApplicationContext().getPackageName() + ".provider";
-            Uri imageUri = FileProvider.getUriForFile(this, authorities, imagen);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        } else {
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
-        }
-        startActivityForResult(intent, COD_FOTO);
-    }
 }
